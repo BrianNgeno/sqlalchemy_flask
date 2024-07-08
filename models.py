7b064af3cda565487923fa4a557e04a5017f9038
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2024-07-08 16:28:41 +0300

    updated check password hash function

--- models.py
+++ models.py
@@ -39,7 +39,7 @@ class User(db.Model,SerializerMixin):
 
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+        return bcrypt.check_password_hash(self._password_hash, password)
 
 
 class Car(db.Model, SerializerMixin):
